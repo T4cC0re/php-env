301403c7564021f5e02ec036cbc4f223160402fb
Hendrik Jonas Meyer <hmeyer@bigpoint.net>
2016-03-11 21:30:05 +0100

    [RUN] Dumbed down run script.

--- scripts/php7.sh
+++ scripts/php7.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-$(realpath $(dirname $0))/run.sh -v 7 php ${@}
+$(realpath $(dirname $0))/run.sh 7 `pwd` 0 php ${@}
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,39 +1,32 @@
 #!/usr/bin/env bash
 
 usage() {
-  echo -e "Usage:\n  ${0} [-v =5.6] [-r =$(pwd)] [-h] [-p]\n  -v: Version of php to run (5.4, 5.5, 5.6, 7)\n  -r: root folder of the application\n  -h: This info text";
+  echo -e "Usage:\n  ${0} <version> <workdir> <layers-up>\n  version: Version of php to run (5.4, 5.5, 5.6, 7)\n  workdir: workdir to mount and set\n  layers-up: Amount of folders upwards the tree to mount\n\nExample:\n  ${0} 5.6 /home/user/project 1 -a\nfor a php 5.6 interactive shell in /home/user/project with having /home/user mounted";
 }
 
-while getopts v:r:h o; do
-  case ${o} in
-    v|--version)
-      VERSION="$OPTARG"
-      if [[ ${VERSION} =~ ^[0-9\.]{1,3}$ ]]; then
-        shift 2
-      else
-        echo "Version must match 'X.Y' or 'X'. E.g. '5.6' or '7'"
-        usage
-        exit 1
-      fi
-      ;;
-    r|--root)
-      ROOT="$OPTARG"
-      shift 2
-      ;;
-    h|--help)
-      usage
-      exit 0
-      ;;
-    :)
-      usage
-      exit 1
-      ;;
-esac
-done
+VERSION="${1}"
+if [[ ${VERSION} =~ ^[0-9\.]{1,3}$ ]]; then
+  shift
+else
+  echo "Version must match 'X.Y' or 'X'. E.g. '5.6' or '7'"
+  usage
+  exit 1
+fi
 
-ROOT="${ROOT:="$(pwd)"}"
-VERSION="${VERSION:="5.6"}"
+ROOT="${1}"
+shift
+
+LAYERS="${1}"
+if [ "${LAYERS}" != "0" ]; then
+  for ((i=0; i<$LAYERS; i++)); do
+    UPWARDS="${UPWARDS}../"
+  done
+fi
+MOUNTROOT="$(realpath $(realpath ${ROOT})/${UPWARDS})"
+echo $MOUNTROOT
+shift
+
 CMD="${@}"
 
-echo "Starting PHP ${VERSION} in '${ROOT}' with CMD: '${CMD}'"
-docker run -itp 80:80 -p 443:443 -v "${ROOT}:${ROOT}:rw" -w "${ROOT}" php-env:${VERSION} ${CMD}
+#echo "Starting PHP ${VERSION} in '${ROOT}' with CMD: '${CMD}'"
+docker run -ip 80:80 -p 443:443 -v "${MOUNTROOT}:${MOUNTROOT}:rw" -v "/tmp:/tmp:rw" -w "${ROOT}" php-env:${VERSION} ${CMD}
